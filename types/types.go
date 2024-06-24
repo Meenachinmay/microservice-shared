4d32be2e5aebd32c861bcb023a7138fdd66d4519
meenachinmay <chinmayanand896@gmail.com>
2024-06-24 13:40:40 +0900

    preferred_timings -> preferred_method

--- types/types.go
+++ types/types.go
@@ -3,7 +3,7 @@ package types
 type EnquiryPayload struct {
 	UserID           int32  `json:"user_id"`
 	AvailabelTimings string `json:"available_timings"`
-	PreferredTimings string `json:"preferred_timings"`
+	PreferredMethod string `json:"preferred_method"`
 	PropertyID       int32  `json:"property_id"`
 	PropertyName     string `json:"property_name"`
 	PropertyLocation string `json:"property_location"`
