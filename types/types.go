cd4723ec04e9ea539d775ffdac2a61508d0130bf
meenachinmay <chinmayanand896@gmail.com>
2024-06-24 23:17:16 +0900

    added email to type enquirypayload

--- types/types.go
+++ types/types.go
@@ -3,7 +3,8 @@ package types
 type EnquiryPayload struct {
 	UserID           int32  `json:"user_id"`
 	AvailabelTimings string `json:"available_timings"`
-	PreferredMethod string `json:"preferred_method"`
+	Email            string `json:"email"`
+	PreferredMethod  string `json:"preferred_method"`
 	PropertyID       int32  `json:"property_id"`
 	PropertyName     string `json:"property_name"`
 	PropertyLocation string `json:"property_location"`
