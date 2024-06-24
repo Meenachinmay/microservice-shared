d8400e5812dbed7dfc026baf9698ecc70a5334a8
meenachinmay <chinmayanand896@gmail.com>
2024-06-24 13:17:37 +0900

    added LogPayload type

--- types/types.go
+++ types/types.go
@@ -1,8 +1,16 @@
 package types
 
 type EnquiryPayload struct {
-	UserID     int32  `json:"user_id"`
-	PropertyID int32  `json:"property_id"`
-	Name       string `json:"name"`
-	Location   string `json:"location"`
-	FudousanID int32  `json:"fudousan_id"`
+	UserID           int32  `json:"user_id"`
+	AvailabelTimings string `json:"available_timings"`
+	PreferredTimings string `json:"preferred_timings"`
+	PropertyID       int32  `json:"property_id"`
+	PropertyName     string `json:"property_name"`
+	PropertyLocation string `json:"property_location"`
+	FudousanID       int32  `json:"fudousan_id"`
+}
+
+type LogPayload struct {
+	ServiceName string
+	LogData     string
+}
